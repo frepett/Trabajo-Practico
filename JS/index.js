afe1181c11a2a3c8bedf1968188353eea26b40db
frepett <frepettoechave@udesa.edu.ar>
2024-11-13 20:31:26 -0300

    Lo que hicimos recien en Java

--- JS/index.js
+++ JS/index.js
@@ -5,9 +5,22 @@ fetch('https://dummyjson.com/recipes')
 .then(
     function(data){
         console.log(data);
+        let menu = document.querySelector(".recipes")
+        let titulo =""
+        for (let i=0; i<data.recipes.length; i++){
+            console.log(i)
+            titulo = titulo + `<article>
+            <img src=${data.recipes[i].image} alt = "">
+            <a href="./index.html?id=${data.recipes[i].id}">Plato:${data.recipes[i].name}</a>
+            <p>Ingredientes:${data.recipes[i].ingredients}</p>
+            <p>Instrucciones:${data.recipes[i].instructions}</p>
+            <p>Dificultad: ${data.recipes[i].difficulty}</p>
+            <article>`
+        } 
+        menu.innerHTML = titulo
     }
-)
 
+)
 .catch(function(error)
 {
     console.log(error)
