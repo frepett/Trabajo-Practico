88a21621b898318bab53d20c47d476f37523075b
BautistaSaenzValiente <bsaenzvaliente@udesa.com.ar>
2024-11-14 16:25:37 -0300

    lo que hicimos recien

--- JS/index.js
+++ JS/index.js
@@ -9,11 +9,12 @@ fetch('https://dummyjson.com/recipes')
         let titulo =""
         for (let i=0; i<10; i++){
             console.log(i)
-            titulo = titulo + `<article>
+            titulo += `<article>
             <img src=${data.recipes[i].image} alt = "">
-            <a href="./index.html?id=${data.recipes[i].id}">Plato:${data.recipes[i].name}</a>
-            <p>Dificultad: ${data.recipes[i].difficulty}</p>
-            <article>`;
+            <a class="plato" href="./index.html?id=${data.recipes[i].id}">
+            Plato: ${data.recipes[i].name}</a>
+            <p >Dificultad: ${data.recipes[i].difficulty}</p>
+            </article>`;
         } 
         menu.innerHTML = titulo
     }
