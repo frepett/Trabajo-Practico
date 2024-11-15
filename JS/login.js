7988791e81581b1fdcf723e3ccb0f1a237dcbb0f
LorenzoFernandezA <lfernandezarancibia@udesa.edu.ar>
2024-11-15 09:42:45 -0300

    Tratando de perfeccionar el login

--- JS/login.js
+++ JS/login.js
@@ -1,13 +1,18 @@
-let email = document.getElementById('email').value;
-let password = document.getElementById('password').value;
+let formulario = document.querySelector("form");
+let email = document.querySelector("#email")
+let password = document.querySelector("#password")
+let errorEmail = document.querySelector('.email')
+let errorPassword = document.querySelector('.password')
 
 
-let boton= document.querySelector(`.boton_login`)
-boton.addEventListener("click",function(){
-
-    if (email === "") {
-        alert("El campo de email no puede estar vacío");
-    } else if(password===""){
-        alert("El campo de contraseña no puede estar vacío");
+formulario.addEventListener("submit", function(event){
+    event.preventDefault()
+    if(email.value == ''){
+        alert("Por favor complete el campo de email")
+    }
+    else if(password.value == ''){
+        alert("Por favor complete el campo de email")
+    } else{
+        formulario.submit()
     }
 })
