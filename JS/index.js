64830684fd5e641116d05dc63cd72c3275fbffd2
LorenzoFernandezA <lfernandezarancibia@udesa.edu.ar>
2024-11-15 09:55:28 -0300

    Hize lo del boton cargar mas

--- JS/index.js
+++ JS/index.js
@@ -20,6 +20,18 @@ fetch('https://dummyjson.com/recipes')
         } 
         menu.innerHTML=titulo
         
+        let cargarmas = document.querySelector('.button_cargarmas')
+        cargarmas.addEventListener('click', function(){
+            for(let i=0;i<10;i++){
+                titulo += `<article class="article_de_fotos">
+                 <img src=${data.recipes[i].image} alt = "" >
+                 <a class="plato" href="./index.html?id=${data.recipes[i].id}">
+                 Plato: ${data.recipes[i].name}</a>
+                 <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p>
+                </article>`;
+                menu.innerHTML=titulo
+            }
+        })
     }
 )
 .catch(function(error)
