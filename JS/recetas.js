3b299d0770cbc3b235e9586e8f5d6fd1b87d7aac
frepett <frepettoechave@udesa.edu.ar>
2024-11-15 15:13:22 -0300

    arregle el css de los resultados del buscador

--- JS/recetas.js
+++ JS/recetas.js
@@ -13,11 +13,11 @@ fetch(`https://dummyjson.com/recipes/search?q=${extraer}`)
     console.log(data);
  let contenedorRecetas=document.querySelector(".seccion_detalles")
  for(let i = 0; i<data.recipes.length; i++){
-    contenedorRecetas.innerHTML += `<article class="article_de_fotos">
-                 <img src=${data.recipes[i].image} alt = "" ><div>
-                 <a class="plato" href="./index.html?id=${data.recipes[i].id}">
-                 Plato: ${data.recipes[i].name}</a>
-                 <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></div>
+    contenedorRecetas.innerHTML += `<article class="article_de_fotos_recetas_solo">
+                 <img src=${data.recipes[i].image} alt = "" class="imagen_recetas_sola"><article class="articulo_titulo_dificultad">
+                 <b><a class="plato_receta_solo" href="./index.html?id=${data.recipes[i].id}">
+                 Plato: ${data.recipes[i].name}</a></b>
+                 <p class="dificultad_receta_solo" >Dificultad: ${data.recipes[i].difficulty}</p></article>
                 </article>`;
     
 
