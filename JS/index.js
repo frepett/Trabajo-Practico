bc00e18f2e4ac76ff512dcb73af3ea3b52b0774c
frepett <frepettoechave@udesa.edu.ar>
2024-11-19 19:27:24 -0300

    centre le log in y el registro y agrege buscardor a detalles, hice otros detalles mas, falta categorias y el detalle de cada receta

--- JS/index.js
+++ JS/index.js
@@ -19,21 +19,22 @@ fetch('https://dummyjson.com/recipes')
             
         } 
         menu.innerHTML=titulo
+
         
         let cargarmas = document.querySelector('.button_cargarmas')
         cargarmas.addEventListener('click', function(){
             for(let i=10;i<20;i++){
                 titulo += `<article class="article_de_fotos">
-                 <img src=${data.recipes[i].image} alt = "" ><div>
-                 <a class="plato" href="./index.html?id=${data.recipes[i].id}">
+                 <img src=${data.recipes[i].image} alt = "" >
+                <article class="article_foto_plato"><a class="plato" href="./index.html?id=${data.recipes[i].id}">
                  Plato: ${data.recipes[i].name}</a>
-                 <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></div>
+                 <p class="dificultad">Dificultad: ${data.recipes[i].difficulty}</p></article>
                 </article>`;
-                menu.innerHTML=titulo
             }
+            menu.innerHTML=titulo
         })
-    }
-)
+})
+
 .catch(function(error)
 {
     console.log(error)
