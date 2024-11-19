233f47dc2291f09886ebde23408ab796e5a24f8a
LorenzoFernandezA <lfernandezarancibia@udesa.edu.ar>
2024-11-19 13:47:06 -0300

    Mismo footer en todos los htmls, no logro que salten los alerts del login

--- JS/login.js
+++ JS/login.js
@@ -4,14 +4,18 @@ let password = document.querySelector("#password")
 let errorEmail = document.querySelector('.email')
 let errorPassword = document.querySelector('.password')
 
+console.log(formulario);
+console.log(email);
 
-formulario.addEventListener("submit", function(event){
+formulario.addEventListener('submit', function(event){
     event.preventDefault()
     if(email.value == ''){
-        alert("Por favor complete el campo de email")
+        console.log('Por favor complete el campo de email')
+        errorEmail.innerHTML = '<p>Por favor complete el campo de email</p>'
     }
     else if(password.value == ''){
-        alert("Por favor complete el campo de email")
+        console.log('Por favor complete el campo de contraseña')
+        errorPassword.innerHTML = '<p>Por favor complete el campo de contraseña</p>'
     } else{
         formulario.submit()
     }
