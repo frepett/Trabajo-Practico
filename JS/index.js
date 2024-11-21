9790260fadd1b7f333d894396fba380a6d7836f0
frepett <frepettoechave@udesa.edu.ar>
2024-11-20 23:42:59 -0300

    pido perdon, estaba mal, funcionaba pero el buscador dejaba de andar, borre el codigo

--- JS/index.js
+++ JS/index.js
@@ -43,18 +43,4 @@ fetch('https://dummyjson.com/recipes')
     console.log(error)
 })
 
-let formulario =document.querySelector(".formulario_index")
-let buscador=document.querySelector(".buscador")
-let div_error=document.querySelector(".formulario_index_menos_que3")
-
- formulario.addEventListener("submit",function(evento){
-    evento.preventDefault()
-    if (buscador.value.length<3){
-        div_error.style.display="block"
-        div_error.innerHTML="<p>La busqueda tiene que tener mino 3 caracteres</p>"
-
-    }
-
- }
-)
 
